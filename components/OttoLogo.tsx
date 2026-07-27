c2e75bb9c004d07058b49424a2dd1602bef88e37
JoshuaAguilar414 <dev@vectra-intl.com>
2026-07-27 01:23:02 -0400

    fix: logo

--- components/OttoLogo.tsx
+++ components/OttoLogo.tsx
@@ -9,7 +9,7 @@ export default function OttoLogo({
     // eslint-disable-next-line @next/next/no-img-element
     <img
       className={`otto-logo ${className}`.trim()}
-      src="/otto-group-logo.png"
+      src="https://static.ottogroup.com/wLayout22/wGlobal/layout/images/logo.svg"
       alt={title}
       width={340}
       height={96}
